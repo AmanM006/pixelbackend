d8a904a7a9a9a9309ab5ac6ec58dd58a6af193e1
Aman <82795889+AmanM006@users.noreply.github.com>
2025-04-01 00:03:44 +0530

    Create Post.js

---
+++ models/Post.js
@@ -0,0 +1,18 @@
+// const mongoose = require("mongoose");
+
+// const categorySchema = new mongoose.Schema({
+//     category: { type: String, required: true },  // e.g., "Gaming"
+//     thread: { type: String, required: true },    // e.g., "Best Retro Games"
+//     user: { type: String, required: true },      // Username
+//     message: { type: String, required: true },   // Post content
+//     replies: [
+//         {
+//             user: String,
+//             message: String,
+//             timestamp: { type: Date, default: Date.now }
+//         }
+//     ],
+//     timestamp: { type: Date, default: Date.now }
+// });
+
+// module.exports = mongoose.model("Post", postSchema);
